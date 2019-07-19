3560bfddcecda655ba6ce1d0a0d24b697c251c15
glopez <gopezlaston@gmail.com>
2019-07-19 16:23:53 -0300

    Octave tutorial

---
+++ octave_tutorial.m
@@ -0,0 +1,18 @@
+B = pi;
+disp (sprintf('%0.2f', a))
+format long;
+a
+format short;
+a
+A = [1 2; 3 4]
+Vector = [1; 2; 3]
+v = 1:0.1:2 %Desde 1 hasta 2 con paso 0.1
+C = ones(2,4)
+D = zeros (5,4)
+rand (3,3) #Uniformemente distribuidos entre 0 y 1.
+randn (4,5) #Normal gausseano
+sqrt(4)
+%Histograma
+hist (A, 5)
+help eye
+eye(4)
